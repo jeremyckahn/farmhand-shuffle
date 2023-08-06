6c693c5d1fd134d63912b78af80436b629c0c191
Jeremy Kahn <jeremyckahn@gmail.com>
2023-08-06 18:06:17 -0500

    test(factory): validate buildField

---
+++ src/logic/game/type-guards/index.ts
@@ -0,0 +1,8 @@
+import { Field } from '../types'
+
+export const isField = (obj: unknown): obj is Field => {
+  if (typeof obj !== 'object' || obj === null) return false
+
+  // FIXME: Verify that crops contains only Crop or undefined
+  return 'crops' in obj && Array.isArray(obj['crops'])
+}
