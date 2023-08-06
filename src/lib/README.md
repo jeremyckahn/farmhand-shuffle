876658b36765d231cfa3154dd0652f9d80c9016b
Jeremy Kahn <jeremyckahn@gmail.com>
2023-08-06 17:15:50 -0500

    feat(game): define high-level interfaces

---
+++ src/lib/README.md
@@ -0,0 +1,3 @@
+# lib
+
+This directory contains code that is needed by Farmhand Shuffle, but is not specific to the project. This includes solutions for general needs such as DOM manipulation or standard algorithms and data structures.
---
+++ src/logic/game/types/index.ts
@@ -0,0 +1,111 @@
+import { uuidString } from 'lib/types/index'
+// NOTE: Most of the game's interface properties are readonly to enforce
+// immutability.
+
+export enum CardType {
+  CROP = 'CROP',
+  EVENT = 'EVENT',
+  TOOL = 'TOOL',
+  WATER = 'WATER',
+}
+
+export interface Card {
+  readonly id: string
+  readonly type: CardType
+}
+
+export interface Crop extends Card {
+  readonly type: CardType.CROP
+
+  /**
+   * How many water cards that need to be attached to this Crop card in order
+   * to mature from from a seed to a sellable crop.
+   */
+  readonly waterCardsToMature: number
+}
+
+/**
+ * A stateful representation of a Crop card that is in the Field.
+ */
+export interface PlayedCrop extends Crop {
+  /**
+   * How many water cards are attached to this card.
+   */
+  waterCards: number
+}
+
+/**
+ * Players can play up to one Event card per turn. Has some sort of effect on
+ * one or both players simultaneously, defined per card.
+ */
+export interface Event extends Card {
+  readonly type: CardType.EVENT
+}
+
+/**
+ * Players can play an unlimited number of Tool cards per turn. Some tool cards
+ * can be played from a player's hand during the opponent's turn (this would be
+ * specified on the card). Has some sort of effect, defined per card.
+ */
+export interface Tool extends Card {
+  readonly type: CardType.TOOL
+}
+
+/**
+ * Used to water Crop cards to mature them.
+ */
+export interface Water extends Card {
+  readonly type: CardType.WATER
+}
+
+export interface Field {
+  /**
+   * Sparse array.
+   */
+  readonly Crops: (Crop | undefined)[]
+}
+
+export interface Player {
+  readonly id: uuidString
+
+  /**
+   * The number of coins this player has.
+   */
+  readonly funds: number
+
+  /**
+   * Cards that the player can draw from.
+   */
+  readonly deck: Card['id'][]
+
+  /**
+   * Cards in the player's hand.
+   */
+  readonly hand: Card['id'][]
+
+  /**
+   * Cards that have been used.
+   */
+  readonly discardPile: Card['id'][]
+
+  /**
+   * Cards in the player's Field.
+   */
+  readonly field: Field
+}
+
+export interface Table {
+  /**
+   * Each players' card area at the table.
+   */
+  readonly players: Record<Player['id'], Player>
+
+  /**
+   * The number of coins in the community fund.
+   */
+  readonly communityFund: number
+}
+
+export interface Game {
+  readonly table: Table
+}
