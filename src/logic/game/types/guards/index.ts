4b0017d0cd480fe0c3dfcc1f44e7bb14c12f6894
Jeremy Kahn <jeremyckahn@gmail.com>
2023-08-06 18:16:36 -0500

    feat(game): add isCrop type guard

--- src/logic/game/types/guards/index.ts
+++ src/logic/game/types/guards/index.ts
@@ -1,8 +1,22 @@
-import { Field } from '../index'
+import { CardType, Crop, Field } from '../index'
+
+export const isCrop = (obj: unknown): obj is Crop => {
+  if (typeof obj !== 'object' || obj === null) return false
+
+  return (
+    'id' in obj &&
+    typeof obj.id === 'string' &&
+    'type' in obj &&
+    obj.type === CardType.CROP
+  )
+}
 
 export const isField = (obj: unknown): obj is Field => {
   if (typeof obj !== 'object' || obj === null) return false
 
-  // FIXME: Verify that crops contains only Crop or undefined
-  return 'crops' in obj && Array.isArray(obj['crops'])
+  return (
+    'crops' in obj &&
+    Array.isArray(obj.crops) &&
+    obj.crops.every(crop => isCrop(crop) || crop === undefined)
+  )
 }
