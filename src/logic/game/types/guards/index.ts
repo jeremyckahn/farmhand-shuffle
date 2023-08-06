414f539ed9abe622e968aea7cba595148a0b96b8
Jeremy Kahn <jeremyckahn@gmail.com>
2023-08-06 18:25:31 -0500

    test(factory): validate buildPlayer

--- src/logic/game/types/guards/index.ts
+++ src/logic/game/types/guards/index.ts
@@ -1,4 +1,4 @@
-import { CardType, Crop, Field } from '../index'
+import { CardType, Crop, Field, Player } from '../index'
 
 export const isCrop = (obj: unknown): obj is Crop => {
   if (typeof obj !== 'object' || obj === null) return false
@@ -18,5 +18,27 @@ export const isField = (obj: unknown): obj is Field => {
     'crops' in obj &&
     Array.isArray(obj.crops) &&
     obj.crops.every(crop => isCrop(crop) || crop === undefined)
+  )
+}
+
+export const isPlayer = (obj: unknown): obj is Player => {
+  if (typeof obj !== 'object' || obj === null) return false
+
+  return (
+    'id' in obj &&
+    typeof obj.id === 'string' &&
+    'funds' in obj &&
+    typeof obj.funds === 'number' &&
+    'deck' in obj &&
+    Array.isArray(obj.deck) &&
+    obj.deck.every(card => typeof card === 'string') &&
+    'hand' in obj &&
+    Array.isArray(obj.hand) &&
+    obj.hand.every(card => typeof card === 'string') &&
+    'discardPile' in obj &&
+    Array.isArray(obj.discardPile) &&
+    obj.discardPile.every(card => typeof card === 'string') &&
+    'field' in obj &&
+    isField(obj.field)
   )
 }
