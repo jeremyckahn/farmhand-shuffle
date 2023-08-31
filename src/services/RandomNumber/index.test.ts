dc1e65d30644705b01c9d51532f06b6247a3974a
Jeremy Kahn <jeremyckahn@gmail.com>
2023-08-31 09:39:15 -0500

    feat(services): implement randomIndex and chooseElement

--- src/services/RandomNumber/index.test.ts
+++ src/services/RandomNumber/index.test.ts
@@ -1,11 +1,33 @@
 import { RandomNumber } from '.'
 
 describe('RandomNumber', () => {
-  test('generates random number', () => {
-    jest.spyOn(Math, 'random').mockReturnValueOnce(0.5)
+  describe('generate', () => {
+    test('generates random number', () => {
+      jest.spyOn(Math, 'random').mockReturnValueOnce(0.5)
 
-    const generatedNumber = RandomNumber.generate()
+      const generatedNumber = RandomNumber.generate()
 
-    expect(generatedNumber).toEqual(0.5)
+      expect(generatedNumber).toEqual(0.5)
+    })
+  })
+
+  describe('randomIndex', () => {
+    test('choses a random index from an array', () => {
+      jest.spyOn(RandomNumber, 'generate').mockReturnValueOnce(0.5)
+
+      const selectedIndex = RandomNumber.randomIndex([1, 2, 3])
+
+      expect(selectedIndex).toEqual(1)
+    })
+  })
+
+  describe('chooseElement', () => {
+    test('choses a random element from an array', () => {
+      jest.spyOn(RandomNumber, 'generate').mockReturnValueOnce(0.5)
+
+      const selectedIndex = RandomNumber.chooseElement([1, 2, 3])
+
+      expect(selectedIndex).toEqual(2)
+    })
   })
 })
--- src/services/RandomNumber/index.ts
+++ src/services/RandomNumber/index.ts
@@ -2,4 +2,12 @@ export class RandomNumber {
   static generate() {
     return Math.random()
   }
+
+  static randomIndex(list: any[]) {
+    return Math.round(RandomNumber.generate() * (list.length - 1))
+  }
+
+  static chooseElement(list: any[]) {
+    return list[RandomNumber.randomIndex(list)]
+  }
 }
