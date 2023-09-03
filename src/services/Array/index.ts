6e9be2ad50948c41c697624e9fba591913461d82
Jeremy Kahn <jeremyckahn@gmail.com>
2023-09-03 17:06:02 -0500

    refactor(ArrayService): simplify removeAt

--- src/services/Array/index.ts
+++ src/services/Array/index.ts
@@ -4,6 +4,6 @@ export class ArrayService {
       throw new Error(`removeAt: Index ${idx} out of bounds`)
     }
 
-    return [...array.slice(0, idx), ...array.slice(idx + 1, array.length)]
+    return [...array.slice(0, idx).concat(array.slice(idx + 1))]
   }
 }
