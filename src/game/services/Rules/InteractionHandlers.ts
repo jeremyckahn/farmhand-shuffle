9b745b9882af8028b56730314b7ab2ddafed3398
Jeremy Kahn <jeremyckahn@gmail.com>
2023-09-24 15:35:34 -0500

    docs(InteractionHandlers): explain intent

--- src/game/services/Rules/InteractionHandlers.ts
+++ src/game/services/Rules/InteractionHandlers.ts
@@ -1,5 +1,10 @@
 import { IGame, IPlayer } from '../../types'
 
+/**
+ * This interface is the sole connecting mechanism between the user-facing app
+ * and the core game logic that are otherwise completely isolated from one
+ * another.
+ */
 export interface InteractionHandlers {
   /**
    * Returns the index of the selected card in the IField array.
