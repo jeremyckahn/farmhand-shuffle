abb1a901da976e88bca7cd83ac60f4dfcdecb88e
Jeremy Kahn <jeremyckahn@gmail.com>
2023-09-26 09:53:55 -0500

    feat(app): set up MUI

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,3 +1,12 @@
+import Paper from '@mui/material/Paper'
+import useTheme from '@mui/material/styles/useTheme'
+
 export const Card = () => {
-  return <></>
+  const theme = useTheme()
+
+  return (
+    <Paper
+      sx={{ width: 300, height: 500, background: theme.palette.grey[200] }}
+    ></Paper>
+  )
 }
--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Card } from '../app/components/Card'
 
+// FIXME: Integrate MUI: https://storybook.js.org/recipes/@mui/material
+
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Farmhand Shuffle/Card',
