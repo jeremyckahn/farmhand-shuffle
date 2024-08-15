1468b7cd5fa6341299921208a018c5424b78f526
Jeremy Kahn <me@jeremyckahn.com>
2024-08-15 08:44:53 -0500

    feat(ui): give 3D perspective to card flip presentation

--- src/app/components/Card/CardTemplate.tsx
+++ src/app/components/Card/CardTemplate.tsx
@@ -33,100 +33,108 @@ export const CardTemplate = ({
 
   return (
     <Box
-      className={cardClassName}
-      sx={[
-        {
-          height: CARD_DIMENSIONS[size].height,
-          position: 'relative',
-          transformStyle: 'preserve-3d',
-          transition: theme.transitions.create(['transform']),
-          width: CARD_DIMENSIONS[size].width,
-          ...(isFlipped && {
-            transform: 'rotateY(180deg)',
-          }),
-        },
-        ...(Array.isArray(sx) ? sx : [sx]),
-      ]}
-      {...props}
+      sx={{
+        perspective: '1000px',
+        height: CARD_DIMENSIONS[size].height,
+        width: CARD_DIMENSIONS[size].width,
+      }}
     >
-      <Paper
-        {...paperProps}
+      <Box
+        className={cardClassName}
         sx={[
           {
-            backfaceVisibility: 'hidden',
-            background:
-              theme.palette.mode === 'light'
-                ? darken(theme.palette.background.paper, 0.05)
-                : lighten(theme.palette.background.paper, 0.15),
-            display: 'flex',
-            flexDirection: 'column',
             height: 1,
-            p: theme.spacing(1),
-            position: 'absolute',
+            position: 'relative',
+            transformStyle: 'preserve-3d',
+            transition: theme.transitions.create(['transform']),
             width: 1,
+            ...(isFlipped && {
+              transform: 'rotateY(180deg)',
+            }),
           },
+          ...(Array.isArray(sx) ? sx : [sx]),
         ]}
+        {...props}
       >
-        <Typography
-          variant="overline"
-          sx={{ fontWeight: theme.typography.fontWeightBold }}
+        <Paper
+          {...paperProps}
+          sx={[
+            {
+              backfaceVisibility: 'hidden',
+              background:
+                theme.palette.mode === 'light'
+                  ? darken(theme.palette.background.paper, 0.05)
+                  : lighten(theme.palette.background.paper, 0.15),
+              display: 'flex',
+              flexDirection: 'column',
+              height: 1,
+              p: theme.spacing(1),
+              position: 'absolute',
+              width: 1,
+            },
+          ]}
         >
-          {card.name}
-        </Typography>
-        <Box
-          sx={{
-            height: '50%',
-            display: 'flex',
-            background: theme.palette.common.white,
-            backgroundImage: `url(${ui.dirt})`,
-            backgroundSize: '100%',
-            backgroundRepeat: 'repeat',
-            borderColor: theme.palette.divider,
-            borderRadius: `${theme.shape.borderRadius}px`,
-            borderWidth: 1,
-            borderStyle: 'solid',
-            imageRendering: 'pixelated',
-          }}
-        >
-          <Image
-            src={imageSrc}
-            alt={card.name}
+          <Typography
+            variant="overline"
+            sx={{ fontWeight: theme.typography.fontWeightBold }}
+          >
+            {card.name}
+          </Typography>
+          <Box
             sx={{
-              height: `${100 * imageScale}%`,
-              p: 0,
-              m: 'auto',
+              height: '50%',
+              display: 'flex',
+              background: theme.palette.common.white,
+              backgroundImage: `url(${ui.dirt})`,
+              backgroundSize: '100%',
+              backgroundRepeat: 'repeat',
+              borderColor: theme.palette.divider,
+              borderRadius: `${theme.shape.borderRadius}px`,
+              borderWidth: 1,
+              borderStyle: 'solid',
               imageRendering: 'pixelated',
-              filter: `drop-shadow(0 0 5px ${theme.palette.common.white})`,
             }}
-          />
-        </Box>
-        <Divider sx={{ my: theme.spacing(1) }} />
-        <Box sx={{ height: '50%' }}>{children}</Box>
-      </Paper>
-      <Paper
-        {...paperProps}
-        sx={{
-          alignItems: 'center',
-          backfaceVisibility: 'hidden',
-          display: 'flex',
-          height: 1,
-          position: 'absolute',
-          textAlign: 'center',
-          transform: 'rotateY(180deg)',
-          width: 1,
-        }}
-      >
-        <Typography
-          variant="h1"
+          >
+            <Image
+              src={imageSrc}
+              alt={card.name}
+              sx={{
+                height: `${100 * imageScale}%`,
+                p: 0,
+                m: 'auto',
+                imageRendering: 'pixelated',
+                filter: `drop-shadow(0 0 5px ${theme.palette.common.white})`,
+              }}
+            />
+          </Box>
+          <Divider sx={{ my: theme.spacing(1) }} />
+          <Box sx={{ height: '50%' }}>{children}</Box>
+        </Paper>
+        <Paper
+          {...paperProps}
           sx={{
-            fontSize: theme.typography.h4.fontSize,
-            fontWeight: theme.typography.h4.fontWeight,
-            lineHeight: theme.typography.h4.lineHeight,
+            alignItems: 'center',
+            backfaceVisibility: 'hidden',
+            display: 'flex',
+            height: 1,
+            position: 'absolute',
+            textAlign: 'center',
+            transform: 'rotateY(180deg)',
+            width: 1,
           }}
         >
-          Farmhand Shuffle
-        </Typography>
-      </Paper>
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: theme.typography.h4.fontSize,
+              fontWeight: theme.typography.h4.fontWeight,
+              lineHeight: theme.typography.h4.lineHeight,
+            }}
+          >
+            Farmhand Shuffle
+          </Typography>
+        </Paper>
+      </Box>
     </Box>
   )
 }
