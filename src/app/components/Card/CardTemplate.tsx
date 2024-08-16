9ee407c8b40449a28e3d7769bdbab11b1293f618
Jeremy Kahn <me@jeremyckahn.com>
2024-08-15 21:29:44 -0500

    fix(ui): prevent visual distortion of positioned cards

--- src/app/components/Card/CardTemplate.tsx
+++ src/app/components/Card/CardTemplate.tsx
@@ -32,6 +32,118 @@ export const CardTemplate = ({
     console.error(`Card ID ${card.id} does not have an image configured`)
   }
 
+  const sxArray = Array.isArray(sx) ? sx : [sx]
+  const isBeingTransformed = sxArray.some(({ transform }) => Boolean(transform))
+
+  const baseCard = (
+    <Box
+      className={cardClassName}
+      sx={[
+        {
+          height: CARD_DIMENSIONS[size].height,
+          position: 'relative',
+          transformStyle: 'preserve-3d',
+          transition: theme.transitions.create(['transform']),
+          width: CARD_DIMENSIONS[size].width,
+          ...(isFlipped && {
+            transform: 'rotateY(180deg)',
+          }),
+        },
+        ...sxArray,
+      ]}
+      {...props}
+    >
+      <Paper
+        {...paperProps}
+        sx={[
+          {
+            backfaceVisibility: 'hidden',
+            background:
+              theme.palette.mode === 'light'
+                ? darken(theme.palette.background.paper, 0.05)
+                : lighten(theme.palette.background.paper, 0.15),
+            display: 'flex',
+            flexDirection: 'column',
+            height: 1,
+            p: theme.spacing(1),
+            position: 'absolute',
+            width: 1,
+          },
+        ]}
+      >
+        <Typography
+          variant="overline"
+          sx={{ fontWeight: theme.typography.fontWeightBold }}
+        >
+          {card.name}
+        </Typography>
+        <Box
+          sx={{
+            height: '50%',
+            display: 'flex',
+            background: theme.palette.common.white,
+            backgroundImage: `url(${ui.dirt})`,
+            backgroundSize: '100%',
+            backgroundRepeat: 'repeat',
+            borderColor: theme.palette.divider,
+            borderRadius: `${theme.shape.borderRadius}px`,
+            borderWidth: 1,
+            borderStyle: 'solid',
+            imageRendering: 'pixelated',
+          }}
+        >
+          <Image
+            src={imageSrc}
+            alt={card.name}
+            sx={{
+              height: `${100 * imageScale}%`,
+              p: 0,
+              m: 'auto',
+              imageRendering: 'pixelated',
+              filter: `drop-shadow(0 0 5px ${theme.palette.common.white})`,
+            }}
+          />
+        </Box>
+        <Divider sx={{ my: theme.spacing(1) }} />
+        <Box sx={{ height: '50%' }}>{children}</Box>
+      </Paper>
+      <Paper
+        {...paperProps}
+        sx={{
+          alignItems: 'center',
+          backfaceVisibility: 'hidden',
+          display: 'flex',
+          height: 1,
+          position: 'absolute',
+          textAlign: 'center',
+          transform: 'rotateY(180deg)',
+          width: 1,
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            fontSize: theme.typography.h4.fontSize,
+            fontWeight: theme.typography.h4.fontWeight,
+            lineHeight: theme.typography.h4.lineHeight,
+          }}
+        >
+          Farmhand Shuffle
+        </Typography>
+      </Paper>
+    </Box>
+  )
+
+  // NOTE: If the card is being transformed by the parent component, the
+  // perspective wrapper below will visually distort it. To avoid that, the
+  // base card is rendered directly.
+  if (isBeingTransformed) {
+    return baseCard
+  }
+
+  // NOTE: If the card is not being transformed by the parent component, render
+  // it in a wrapper with styles that will result in a 3D effect to enhance the
+  // card flip animation.
   return (
     <Box
       className={card3DWrapperClassName}
@@ -41,102 +153,7 @@ export const CardTemplate = ({
         width: CARD_DIMENSIONS[size].width,
       }}
     >
-      <Box
-        className={cardClassName}
-        sx={[
-          {
-            height: 1,
-            position: 'relative',
-            transformStyle: 'preserve-3d',
-            transition: theme.transitions.create(['transform']),
-            width: 1,
-            ...(isFlipped && {
-              transform: 'rotateY(180deg)',
-            }),
-          },
-          ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-        {...props}
-      >
-        <Paper
-          {...paperProps}
-          sx={[
-            {
-              backfaceVisibility: 'hidden',
-              background:
-                theme.palette.mode === 'light'
-                  ? darken(theme.palette.background.paper, 0.05)
-                  : lighten(theme.palette.background.paper, 0.15),
-              display: 'flex',
-              flexDirection: 'column',
-              height: 1,
-              p: theme.spacing(1),
-              position: 'absolute',
-              width: 1,
-            },
-          ]}
-        >
-          <Typography
-            variant="overline"
-            sx={{ fontWeight: theme.typography.fontWeightBold }}
-          >
-            {card.name}
-          </Typography>
-          <Box
-            sx={{
-              height: '50%',
-              display: 'flex',
-              background: theme.palette.common.white,
-              backgroundImage: `url(${ui.dirt})`,
-              backgroundSize: '100%',
-              backgroundRepeat: 'repeat',
-              borderColor: theme.palette.divider,
-              borderRadius: `${theme.shape.borderRadius}px`,
-              borderWidth: 1,
-              borderStyle: 'solid',
-              imageRendering: 'pixelated',
-            }}
-          >
-            <Image
-              src={imageSrc}
-              alt={card.name}
-              sx={{
-                height: `${100 * imageScale}%`,
-                p: 0,
-                m: 'auto',
-                imageRendering: 'pixelated',
-                filter: `drop-shadow(0 0 5px ${theme.palette.common.white})`,
-              }}
-            />
-          </Box>
-          <Divider sx={{ my: theme.spacing(1) }} />
-          <Box sx={{ height: '50%' }}>{children}</Box>
-        </Paper>
-        <Paper
-          {...paperProps}
-          sx={{
-            alignItems: 'center',
-            backfaceVisibility: 'hidden',
-            display: 'flex',
-            height: 1,
-            position: 'absolute',
-            textAlign: 'center',
-            transform: 'rotateY(180deg)',
-            width: 1,
-          }}
-        >
-          <Typography
-            variant="h1"
-            sx={{
-              fontSize: theme.typography.h4.fontSize,
-              fontWeight: theme.typography.h4.fontWeight,
-              lineHeight: theme.typography.h4.lineHeight,
-            }}
-          >
-            Farmhand Shuffle
-          </Typography>
-        </Paper>
-      </Box>
+      {baseCard}
     </Box>
   )
 }
