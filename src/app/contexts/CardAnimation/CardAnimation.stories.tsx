af703279de06624195fd7ca0e60248213d2be904
Jeremy Kahn <me@jeremyckahn.com>
2024-10-29 08:43:16 -0500

    feat(ui): scaffold drawing a card

--- src/app/contexts/CardAnimation/CardAnimation.stories.tsx
+++ src/app/contexts/CardAnimation/CardAnimation.stories.tsx
@@ -1,5 +1,5 @@
+import { useState } from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
-
 import Box from '@mui/material/Box'
 
 import { lookup } from '../../../game/services/Lookup'
@@ -8,14 +8,20 @@ import { stubGame } from '../../../test-utils/stubs/game'
 import { DiscardPile } from '../../components/DiscardPile'
 import { Field } from '../../components/Field'
 import { Hand } from '../../components/Hand'
-import { Deck } from '../../components/Deck/Deck'
+import { Deck, DeckProps } from '../../components/Deck/Deck'
+import { drawCard } from '../../../game/reducers/draw-card'
 
 import { CardAnimationProvider } from './CardAnimation'
 
-const CardAnimationTestHarness = ({ game }: { game: IGame }) => {
-  const { sessionOwnerPlayerId: userPlayerId } = game
+const CardAnimationTestHarness = ({ game: initialGame }: { game: IGame }) => {
+  const [game, setGame] = useState(initialGame)
+  const { sessionOwnerPlayerId: userPlayerId } = initialGame
 
-  const opponentPlayerIds = lookup.getOpponentPlayerIds(game)
+  const opponentPlayerIds = lookup.getOpponentPlayerIds(initialGame)
+
+  const handleClickTopCard: DeckProps['handleClickTopCard'] = () => {
+    setGame(drawCard(game, userPlayerId))
+  }
 
   return (
     <CardAnimationProvider game={game}>
@@ -24,7 +30,11 @@ const CardAnimationTestHarness = ({ game }: { game: IGame }) => {
         {opponentPlayerIds.map(playerId => {
           return <Field key={playerId} game={game} playerId={playerId} />
         })}
-        <Deck game={game} playerId={userPlayerId} />
+        <Deck
+          game={game}
+          playerId={userPlayerId}
+          handleClickTopCard={handleClickTopCard}
+        />
         <Hand game={game} playerId={userPlayerId} />
         <DiscardPile game={game} playerId={userPlayerId} />
       </Box>
