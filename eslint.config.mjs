081d1741d688446d849a6cc3957dfa8d411a3984
Jeremy Kahn <me@jeremyckahn.com>
2024-10-30 08:57:15 -0500

    chore(project): [wip] [skip ci] use ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import importPlugin from 'eslint-plugin-import'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-curly-brace-presence': ['error', { props: 'never' }],
+      'object-shorthand': 'error',
+      // See: https://typescript-eslint.io/rules/no-unused-vars/#benefits-over-typescript
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always-and-inside-groups',
+        },
+      ],
+    },
+    ignores: ['dist'],
+  },
+]
