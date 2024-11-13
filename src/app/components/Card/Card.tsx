153f0a0611ceb1cba304dfa0670ccb3392467fe6
Jeremy Kahn <me@jeremyckahn.com>
2024-11-12 21:57:03 -0600

    refactor(ui): remove card proxies

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -20,7 +20,6 @@ export interface BaseCardProps extends BoxProps {
   card: ICard
   size?: CardSize
   imageScale?: number
-  isProxy?: boolean
 }
 
 export interface CropCardProps extends BaseCardProps {
--- src/app/components/Card/CardTemplate.tsx
+++ src/app/components/Card/CardTemplate.tsx
@@ -1,6 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { useIsMounted } from 'usehooks-ts'
-import { v4 as uuid } from 'uuid'
+import React, { useRef } from 'react'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import Divider from '@mui/material/Divider'
@@ -13,8 +11,6 @@ import { cards, isCardImageKey, ui } from '../../img'
 import { Image } from '../Image'
 import { CardSize } from '../../types'
 import { isSxArray } from '../../type-guards'
-import { CardAnimationContext } from '../../contexts/CardAnimation/CardAnimation'
-import { sleep } from '../../../lib/sleep'
 
 import { CardProps } from './Card'
 
@@ -28,7 +24,6 @@ export const CardTemplate = React.forwardRef<HTMLDivElement, CardProps>(
       children,
       imageScale = 0.75,
       isFlipped = false,
-      isProxy = false,
       paperProps,
       size = CardSize.MEDIUM,
       sx = [],
@@ -39,69 +34,11 @@ export const CardTemplate = React.forwardRef<HTMLDivElement, CardProps>(
     const theme = useTheme()
     const cardRef = useRef<HTMLDivElement>(null)
 
-    const { mountProxy, unmountProxy, updateProxy } =
-      useContext(CardAnimationContext)
     const imageSrc = isCardImageKey(card.id) ? cards[card.id] : ui.pixel
-    const [instanceId] = useState(() => uuid())
-    const isMounted = useIsMounted()
 
     if (imageSrc === ui.pixel) {
       console.error(`Card ID ${card.id} does not have an image configured`)
     }
-
-    useEffect(() => {
-      if (!isProxy) {
-        mountProxy(card.id, instanceId)
-      }
-
-      return () => {
-        if (!isProxy) {
-          unmountProxy(card.id, instanceId)
-        }
-      }
-    }, [isProxy, instanceId])
-
-    useEffect(() => {
-      if (isProxy) return
-      ;(async () => {
-        await sleep(1)
-
-        if (!isMounted()) return
-
-        const { current: element } = cardRef
-        if (!element) return
-
-        // See: https://stackoverflow.com/a/26893663/470685
-        //const scale =
-        //element.getBoundingClientRect().width / element.offsetWidth
-
-        const { transform } = window.getComputedStyle(element)
-        // See: https://css-tricks.com/get-value-of-css-rotation-through-javascript/
-        const matrix = transform
-          ?.split('(')[1]
-          ?.split(')')[0]
-          ?.split(',')
-          .map(Number)
-
-        if (!matrix) return
-
-        const [a, b] = matrix
-        const scale = Math.sqrt(a * a + b * b)
-        //const rotate = Math.round(Math.atan2(b, a) * (180 / Math.PI))
-
-        const { x, y } = element.getBoundingClientRect()
-        updateProxy({
-          cardId: card.id,
-          instanceId,
-          x,
-          y,
-          scale,
-          // FIXME: Use real rotation values
-          rotateX: 0,
-          rotateY: 0,
-        })
-      })()
-    }, [sx])
 
     return (
       <Box
@@ -112,8 +49,6 @@ export const CardTemplate = React.forwardRef<HTMLDivElement, CardProps>(
             perspective: '1000px',
             height: CARD_DIMENSIONS[size].height,
             width: CARD_DIMENSIONS[size].width,
-            // FIXME: This is WIP
-            ...(isProxy && { visibility: 'hidden' }),
           },
           ...(isSxArray(sx) ? sx : [sx]),
         ]}
@@ -130,12 +65,7 @@ export const CardTemplate = React.forwardRef<HTMLDivElement, CardProps>(
               ...(isFlipped && {
                 transform: 'rotateY(180deg)',
               }),
-              ...(isProxy && {
-                transition: theme.transitions.create([
-                  'transform',
-                  'box-shadow',
-                ]),
-              }),
+              transition: theme.transitions.create(['transform', 'box-shadow']),
             },
           ]}
         >
