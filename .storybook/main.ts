59c9553793fc2ec7a8a6439d4b1bb17b66914d0f
Jeremy Kahn <me@jeremyckahn.com>
2025-02-25 20:12:13 -0600

    chore(deps): use storybook@8.6.0

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -17,9 +17,7 @@ const config: StorybookConfig = {
       strictMode: true,
     },
   },
-  docs: {
-    autodocs: 'tag',
-  },
+  docs: {},
   typescript: {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
