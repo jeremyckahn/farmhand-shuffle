9a709a91c3435f1ffa06b4517090621685a5526d
Jeremy Kahn <me@jeremyckahn.com>
2025-03-10 08:54:52 -0500

    WIP [skip ci] [skip deploy] show alert when watering crop

--- src/ui/components/PlayedCrop/PlayedCrop.tsx
+++ src/ui/components/PlayedCrop/PlayedCrop.tsx
@@ -1,22 +1,71 @@
 import Box, { BoxProps } from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
+import useTheme from '@mui/material/styles/useTheme'
+import { funAnimalName } from 'fun-animal-names'
+import { useContext, useEffect } from 'react'
 
-import { useTheme } from '@mui/material'
-
-import { IPlayedCrop, isCropCardInstance } from '../../../game/types'
+import * as cards from '../../../game/cards'
+import { InvalidCardError } from '../../../game/services/Rules/errors'
+import {
+  CardInstance,
+  IPlayedCrop,
+  IPlayer,
+  isCropCardInstance,
+} from '../../../game/types'
+import { assertIsCardId } from '../../../game/types/guards'
 import { CARD_DIMENSIONS } from '../../config/dimensions'
+import { cards as cardImages } from '../../img'
 import { CardSize } from '../../types'
 import { Card, CropCardProps } from '../Card'
+import { ShellContext } from '../Game/ShellContext'
 import { Image } from '../Image'
-import { cards as cardImages } from '../../img'
-import { InvalidCardError } from '../../../game/services/Rules/errors'
 
 export interface PlayedCropProps extends BoxProps {
-  isInBackground: boolean
   cropCardProps: CropCardProps & { playedCrop: IPlayedCrop }
+  isInBackground: boolean
 }
 
 export const unfilledWaterIndicatorOpacity = 0.25
+
+const WaterIndicator = ({
+  cardInstance,
+  isFilled,
+  opacity,
+  playerId,
+}: {
+  cardInstance: CardInstance
+  isFilled: boolean
+  opacity: number
+  playerId: IPlayer['id']
+}) => {
+  const theme = useTheme()
+  const { showAlert } = useContext(ShellContext)
+
+  assertIsCardId(cardInstance.id)
+
+  const card = cards[cardInstance.id]
+
+  useEffect(() => {
+    if (isFilled) {
+      // FIXME: Update tests
+      // FIXME: Only show this for the opponent players
+      // FIXME: This doesn't always show
+      showAlert(`${funAnimalName(playerId)} watered their ${card.name}`, 'info')
+    }
+  }, [playerId, card, showAlert, isFilled])
+
+  return (
+    <Image
+      src={cardImages.water}
+      alt="Water card indicator"
+      sx={{
+        imageRendering: 'pixelated',
+        opacity,
+        transition: theme.transitions.create(['opacity']),
+      }}
+    />
+  )
+}
 
 export const PlayedCrop = ({
   isInBackground,
@@ -53,22 +102,21 @@ export const PlayedCrop = ({
         {new Array(waterIconsToRender).fill(null).map((_, idx) => {
           let opacity = 1
 
+          const isFilled = idx < playedCrop.waterCards
+
           if (isInBackground) {
             opacity = 0
-          } else if (idx >= playedCrop.waterCards) {
+          } else if (!isFilled) {
             opacity = unfilledWaterIndicatorOpacity
           }
 
           return (
             <Grid key={idx} item sx={{ pt: `${theme.spacing(0)} !important` }}>
-              <Image
-                src={cardImages.water}
-                alt="Water card indicator"
-                sx={{
-                  imageRendering: 'pixelated',
-                  opacity,
-                  transition: theme.transitions.create(['opacity']),
-                }}
+              <WaterIndicator
+                cardInstance={playedCrop.instance}
+                isFilled={isFilled}
+                opacity={opacity}
+                playerId={cropCardProps.playerId}
               />
             </Grid>
           )
