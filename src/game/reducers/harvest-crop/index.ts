5da6b4c67aae8f00ee9ec1fcaf6a03830fb5a7af
Jeremy Kahn <me@jeremyckahn.com>
2025-03-28 09:01:24 -0500

    fixup! feat(game): [wip] implement crop harvesting

--- src/game/reducers/harvest-crop/index.ts
+++ src/game/reducers/harvest-crop/index.ts
@@ -1,12 +1,15 @@
 import { IGame, IPlayer } from '../../types'
+import { moveFromFieldToDiscardPile } from '../move-from-field-to-discard-pile'
 
+// FIXME: Test this
 export const harvestCrop = (
   game: IGame,
   playerId: IPlayer['id'],
   cropIdxInFieldToHarvest: number
 ) => {
-  // FIXME: Implement this
-  console.log({ game, playerId, cropIdxInFieldToHarvest })
+  game = moveFromFieldToDiscardPile(game, playerId, cropIdxInFieldToHarvest)
+
+  // FIXME: Give the player funds
 
   return game
 }
