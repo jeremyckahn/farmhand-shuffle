ae7425a98a89aaf2a179e8266ea468270a35c198
Jeremy Kahn <me@jeremyckahn.com>
2025-04-24 20:41:07 -0500

    feat(app): render game component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import CssBaseline from '@mui/material/CssBaseline'
+
+import { stubDeck } from './test-utils/stubs/deck'
+import { stubPlayer } from './test-utils/stubs/players'
+import { Game } from './ui/components/Game'
+
+// NOTE: This is temporary glue code to be replaced by UX that enables players
+// to build their own decks.
+const deck = stubDeck()
+const player1 = stubPlayer({ deck })
+const player2 = stubPlayer({ deck })
+
 export const App = () => {
-  return <h1>Hello world!</h1>
+  return (
+    <>
+      <CssBaseline />
+      <Game playerSeeds={[player1, player2]} userPlayerId={player1.id} />
+    </>
+  )
 }
