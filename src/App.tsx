14f4754c1cf9cdcc38442e6285d59e33a7400640
Jeremy Kahn <me@jeremyckahn.com>
2025-04-27 17:20:17 -0500

    feat(ui): improve game display in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import CssBaseline from '@mui/material/CssBaseline'
+import ThemeProvider from '@mui/material/styles/ThemeProvider'
 
 import { stubDeck } from './test-utils/stubs/deck'
 import { stubPlayer } from './test-utils/stubs/players'
 import { Game } from './ui/components/Game'
+import { lightTheme } from './ui/theme'
 
 // NOTE: This is temporary glue code to be replaced by UX that enables players
 // to build their own decks.
@@ -12,9 +14,13 @@ const player2 = stubPlayer({ deck })
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
       <CssBaseline />
-      <Game playerSeeds={[player1, player2]} userPlayerId={player1.id} />
-    </>
+      <Game
+        fullHeight
+        playerSeeds={[player1, player2]}
+        userPlayerId={player1.id}
+      />
+    </ThemeProvider>
   )
 }
