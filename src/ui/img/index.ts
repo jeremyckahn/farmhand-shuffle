48067d0dcead5fa2b2a32759ce6adf7216307ddc
Jeremy Kahn <me@jeremyckahn.com>
2025-04-27 11:39:18 -0500

    feat(ui): improve background presentation

--- src/ui/img/index.ts
+++ src/ui/img/index.ts
@@ -3,6 +3,7 @@ import { ICard } from '../../game/types'
 import carrot from './cards/carrot.png'
 import pumpkin from './cards/pumpkin.png'
 import wateringCan from './cards/watering-can.png'
+import brownDotBackground from './ui/brown-dot-bg.png'
 import dirt from './ui/dirt.png'
 import pixel from './ui/pixel.png'
 
@@ -15,6 +16,7 @@ export const cards = {
 export const ui = {
   dirt,
   pixel,
+  brownDotBackground,
 }
 
 export const isCardImageKey = (key: ICard['id']): key is keyof typeof cards => {
