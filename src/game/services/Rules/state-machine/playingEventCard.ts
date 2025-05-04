7d006878fa913a0d94a333f13604e86d56d422e1
Jeremy Kahn <me@jeremyckahn.com>
2025-05-04 14:16:06 -0500

    fixup! feature(game): implement rain card

--- src/game/services/Rules/state-machine/playingEventCard.ts
+++ src/game/services/Rules/state-machine/playingEventCard.ts
@@ -6,28 +6,33 @@ import { assertIsEventCard } from '../../../types/guards'
 
 import { RulesMachineConfig } from './types'
 
-// NOTE: This state serves as a sort of "router" from the generic PLAYING_EVENT
-// state to a more event-specfic state. This centralizes the complexity of
-// determining the appropriate state to transition to when playing an event
-// card.
 export const playingEventCard: RulesMachineConfig['states'] = {
   [GameState.PLAYING_EVENT]: {
     on: {
       [GameEvent.PROMPT_PLAYER_FOR_TURN_ACTION]:
         GameState.WAITING_FOR_PLAYER_TURN_ACTION,
+
+      [GameEvent.PROMPT_BOT_FOR_TURN_ACTION]:
+        GameState.PERFORMING_BOT_TURN_ACTION,
     },
 
     entry: enqueueActions(({ event, context: { game }, enqueue }) => {
       assertEvent(event, GameEvent.PLAY_EVENT)
 
+      const { currentPlayerId, sessionOwnerPlayerId } = game
       const { playerId, cardIdx } = event
       const card = lookup.getCardFromHand(game, playerId, cardIdx)
+
       assertIsEventCard(card)
 
       game = card.applyEffect(game)
 
-      // FIXME: Prompt bot player as necessary
-      enqueue.raise({ type: GameEvent.PROMPT_PLAYER_FOR_TURN_ACTION })
+      if (currentPlayerId === sessionOwnerPlayerId) {
+        enqueue.raise({ type: GameEvent.PROMPT_PLAYER_FOR_TURN_ACTION })
+      } else {
+        enqueue.raise({ type: GameEvent.PROMPT_BOT_FOR_TURN_ACTION })
+      }
+
       enqueue.assign({ game })
     }),
   },
