e752197d342e7daa39254d0e1ba4ed8caaa534e3
Jeremy Kahn <me@jeremyckahn.com>
2025-12-11 20:58:50 -0600

    fixup! Refactor DeckBuilder and address PR comments

--- src/ui/components/DeckBuilder/DeckBuilderSection.tsx
+++ src/ui/components/DeckBuilder/DeckBuilderSection.tsx
@@ -13,7 +13,9 @@ interface DeckBuilderSectionProps {
   cards: ICard[]
   isLast: boolean
   quantities: Record<string, number>
-  onQuantityChange: (cardId: string) => (action: React.SetStateAction<number>) => void
+  onQuantityChange: (
+    cardId: string
+  ) => (action: React.SetStateAction<number>) => void
   totalCards: number
 }
 
@@ -24,27 +26,32 @@ export const DeckBuilderSection = ({
   quantities,
   onQuantityChange,
   totalCards,
-}: DeckBuilderSectionProps) => (
-  <Box key={title}>
-    <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
-      {title}
-    </Typography>
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mb: 2 }}>
-      {cards.map(card => (
-        <CardQuantityControl
-          key={card.id}
-          card={card}
-          quantity={quantities[card.id] || 0}
-          onChange={onQuantityChange(card.id)}
-          cardSize={CardSize.SMALL}
-          isIncreaseDisabled={
+}: DeckBuilderSectionProps) => {
+  return (
+    <Box key={title}>
+      <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
+        {title}
+      </Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mb: 2 }}>
+        {cards.map(card => {
+          const isIncreaseDisabled =
             totalCards >= DECK_SIZE ||
             (card.type !== CardType.WATER &&
               (quantities[card.id] || 0) >= MAX_INSTANCES_PER_CARD)
-          }
-        />
-      ))}
+
+          return (
+            <CardQuantityControl
+              key={card.id}
+              card={card}
+              quantity={quantities[card.id] || 0}
+              onChange={onQuantityChange(card.id)}
+              cardSize={CardSize.SMALL}
+              isIncreaseDisabled={isIncreaseDisabled}
+            />
+          )
+        })}
+      </Box>
+      {!isLast && <Divider sx={{ my: 2 }} />}
     </Box>
-    {!isLast && <Divider sx={{ my: 2 }} />}
-  </Box>
-)
+  )
+}
